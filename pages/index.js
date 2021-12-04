169f8c178baa676e49bb7ff445ac46d41c21af23
Leociao06 <79444333+Leociao06@users.noreply.github.com>
2021-12-04 16:43:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Footer from '@components/Footer
+const ClasseViva = require("classeviva-apiv2").ClasseViva;
 
 export default function Home() {
   return (
